1a6419fba7066c2b0c214ae58d131c783fac84d1
ajkumar874134-create <ajkumar874134@gmail.com>
2026-01-08 00:15:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  if (str.length === 0) return "";
+
+  // Remove leading spaces
+  str = str.trimStart();
+
+  // Find first space
+  const spaceIndex = str.indexOf(" ");
+
+  // If no space found, return entire string
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  // Return substring till first space
+  return str.substring(0, spaceIndex);
 }
+
 
 // Do not change the code below
 
